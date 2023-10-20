f246fcd3af2eeff231813fbc6aa2bc5873ad16d1
Shivangi2430 <103505371+Shivangi2430@users.noreply.github.com>
2023-10-20 14:48:05 +0530

    Time: 11 ms (44.45%), Space: 6.3 MB (25.11%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,9 +3,10 @@ public:
     bool isPalindrome(int x) {
       long long temp = x;
       long long rev = 0;
+        
         while(x>0)
         {
-            int rem = x%10;
+            int rem = x % 10;
             rev = rev*10 + rem;
             x = x/10;
         }

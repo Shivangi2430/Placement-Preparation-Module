442d95d1c455d662f499edf62444f9295c783fcf
Shivangi2430 <103505371+Shivangi2430@users.noreply.github.com>
2023-10-23 17:22:01 +0530

    Time: 3 ms (64.23%), Space: 11.4 MB (20.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -13,16 +13,16 @@ public:
             //check if left part sorted 
             if(nums[low] <= nums[mid])
             {
-                if(nums[low]<=target && target<=nums[mid]) //target is lying in the left half
+                if(nums[low] <= target && target <= nums[mid]) //target is lying in the left half
                 high = mid - 1;
                 else 
                 low = mid + 1;      // target is now lying in the sorted part
             }
 
-            //righ part is sorted
+            //else righ part is sorted
             else
             {
-                 if(nums[mid]<=target && target<=nums[high]) //target is lying in the right half
+                 if(nums[mid] <= target && target <= nums[high]) //target is lying in the right half
                  low = mid + 1;
                  else 
                  high = mid -1;    // target is now lying in the sorted part

c7e7ecbfc0538d2e3356836c10a87f395c77bfda
Shivangi2430 <103505371+Shivangi2430@users.noreply.github.com>
2023-10-24 19:57:05 +0530

    Time: 7 ms (88.27%), Space: 8.4 MB (24.80%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -21,8 +21,8 @@ public:
         
         while(fast!=NULL && fast->next!= NULL)
         {
-            fast = fast->next->next;
             slow = slow->next;
+            fast = fast->next->next;
             if(slow == fast)
                 return true;
         }

8ab007e37aa8f2ef359cf951e1f4a893dac313de
Shivangi2430 <103505371+Shivangi2430@users.noreply.github.com>
2023-11-02 20:35:52 +0530

    Time: 3 ms (46.67%), Space: 6.9 MB (10.81%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     bool backspaceCompare(string s, string t) {
         stack<int>st1, st2;
@@ -15,7 +15,7 @@ public:
             ans1.push_back(st1.top());
             st1.pop();
         }
-       // reverse(ans1.begin(), ans1.end());
+
         
         for(int i = 0; i<t.length(); i++)
         {
@@ -29,8 +29,33 @@ public:
             ans2.push_back(st2.top());
             st2.pop();
         }
-       // reverse(ans2.begin(), ans2.end());
         
         return ans1 == ans2;
+    }
+};
+*/
+
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<int>st1, st2;
+        
+        for(int i = 0; i<s.length(); i++)
+        {
+            if(s[i]=='#' && !st1.empty())
+                st1.pop();
+            else if( s[i] != '#')
+                st1.push(s[i]);
+        }
+        
+        for(int i = 0; i<t.length(); i++)
+        {
+            if(t[i]=='#' && !st2.empty())
+                st2.pop();
+            else if (t[i] != '#')
+                st2.push(t[i]);
+        }
+        
+        return st1 == st2;
     }
 };

32c0b2a04c9cf610846a1473740001a7de52734e
Shivangi2430 <103505371+Shivangi2430@users.noreply.github.com>
2023-12-14 12:36:51 +0530

    Time: 7 ms (88.04%), Space: 16.4 MB (38.55%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,6 +1,61 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+      
+        vector<int>ans;
+        int count1 = 0, count2 = 0;
+        int element1, element2;
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(count1 == 0 && nums[i] != element2)
+            {
+                element1 = nums[i];
+                count1 = 1;
+            }
+            
+            else if(count2 == 0 && nums[i] != element1)
+            {
+                element2 = nums[i];
+                count2 = 1;
+            }
+                
+            else if(element1 == nums[i])
+                count1++;
+            
+            else if(element2 == nums[i])
+                count2++;
+            
+            else
+            {
+                count1--;
+                count2--;
+            }
+        }
+        
+        count1 = 0;
+        count2 = 0;
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i ] == element1)
+                count1++;
+            
+            if(nums[i ] == element2)
+                count2++;
+        }
+        
+        if(count1 > (nums.size()/3))
+            ans.push_back(element1);
+        
+        if(count2 > (nums.size()/3))
+            ans.push_back(element2);
+         
+    return ans;
+        
+        /* TC = O(n)
+           SC = O(n)
+
         unordered_map<int,int>mp;  // element, frequency
         int n = nums.size();
         vector<int>ans;
@@ -16,6 +71,6 @@ public:
             ans.push_back(it.first);
         }
 
-        return ans;
+        return ans;  */
     }
 };

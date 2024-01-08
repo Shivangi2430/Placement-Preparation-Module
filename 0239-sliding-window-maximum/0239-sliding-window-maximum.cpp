cf54f2baa115510d3a53f5af28c9cbb1d08c811d
Shivangi2430 <103505371+Shivangi2430@users.noreply.github.com>
2024-01-08 16:02:40 +0530

    Time: 208 ms (66.35%), Space: 135 MB (60.78%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
    
+         /*Time Complexity: O(N)
+         Space Complexity: O(K)*/
+        
          deque < int > dq;
          vector < int > ans;
          for (int i = 0; i < nums.size(); i++) {
-         if (!dq.empty() && dq.front() == i - k)
+         if (!dq.empty() && dq.front() == i - k) //i-k element will be the out of bound element
              dq.pop_front();
 
-         while (!dq.empty() && nums[dq.back()] < nums[i])
+         while (!dq.empty() && nums[dq.back()] < nums[i]) 
              dq.pop_back();
 
          dq.push_back(i);

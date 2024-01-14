c98f77a29c610b3ce271f87c76a4ca05c7711037
Shivangi2430 <103505371+Shivangi2430@users.noreply.github.com>
2024-01-14 16:41:02 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (81.53%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -27,7 +27,7 @@ SC = O(1)
     //Replace the places in the original array:
     for (int i = 0; i < count0; i++) nums[i] = 0; // replacing 0's
 
-    for (int i = count0; i < count0 + cnt1; i++) nums[i] = 1; // replacing 1's
+    for (int i = count0; i < count0 + count1; i++) nums[i] = 1; // replacing 1's
 
     for (int i = count0 + count1; i < n; i++) nums[i] = 2; // replacing 2's
 

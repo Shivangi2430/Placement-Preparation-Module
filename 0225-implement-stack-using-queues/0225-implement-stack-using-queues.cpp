2fd3a85c7c5d4a411b5b03a10809a3c2da8a011e
Shivangi2430 <103505371+Shivangi2430@users.noreply.github.com>
2024-02-21 14:29:01 +0530

    Time: 3 ms (38.73%), Space: 8.1 MB (57.00%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -2,21 +2,20 @@ class MyStack {
 public:
     queue<int>que;
     MyStack() {
-        
     }
     
     void push(int x) {
         que.push(x);
         for(int i = 0; i < que.size() - 1; i++) //iteration till one size lesser
         {
-            que.push(que.front());
+                que.push(que.front());
                 que.pop();
         }
         
     }
     
     int pop() {
-       int x = que.front();
+        int x = que.front();
         que.pop();
         return x;
     }

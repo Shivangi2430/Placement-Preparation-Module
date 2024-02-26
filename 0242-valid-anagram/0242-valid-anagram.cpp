b42a220fc43ceaea6e6e49e5675c57c7d36a19b2
Shivangi2430 <103505371+Shivangi2430@users.noreply.github.com>
2024-02-26 20:09:30 +0530

    Time: 13 ms (28.33%), Space: 9.8 MB (11.54%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,10 +3,34 @@ public:
     bool isAnagram(string s, string t) {
         // TC = O(n) n = length of the string
         // SC = O(1)
-      if(s.length() != t.length()){
+      if(s.length() != t.length())
+        {
             return false;
         }
-        int arr[26] = {0};
+       unordered_map<char, int> mp;
+
+       for (int i = 0; i < s.length(); i++)
+           mp[s[i]]++;
+
+       for (int i = 0; i < t.length(); i++)
+       {
+           if (mp.find(t[i]) == mp.end() || mp[t[i]] == 0) 
+               return false;
+           
+           else 
+             mp[t[i]]--;    
+       }
+   
+       // Check if all frequencies are zero
+       for (const auto& it : mp)
+       {
+           if (it.second != 0)
+               return false;
+       }
+   
+       return true;
+           
+       /* int arr[26] = {0};
         for(int i=0; i<s.length(); i++){
             arr[s[i]-'a']++;
             arr[t[i]-'a']--;
@@ -16,10 +40,10 @@ public:
                 return false;
             }
         }
-        return true;
+        return true;*/
 
      /* TC = O(nlogn)
-       sort(s.begin(),s.end());
+        sort(s.begin(),s.end());
         sort(t.begin(),t.end());
         if(s==t) return true;
         else return false; 
